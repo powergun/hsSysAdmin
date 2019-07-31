b60880f7dbdafd50181e160686478bf47b553447
Wei Ning <macgnw@gmail.com>
2019-07-31 23:57:05 +1000

    wip

--- scripts/filesystem/walk.hs
+++ scripts/filesystem/walk.hs
@@ -2,15 +2,18 @@
 -- real world haskell P/253
 -- see find / RecursiveContents.hs
 
-import Control.Monad (forM)
+import Control.Monad (forM, forM_)
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
+import Data.List (isSuffixOf)
+
+accept :: FilePath -> Bool
+accept filePath = filePath `notElem` [".", "..", ".git"]
 
 getRecursiveContents :: FilePath -> IO [FilePath]
 getRecursiveContents topDir = do
   names <- getDirectoryContents topDir
-  let properNames = filter (`notElem` [".", ".."]) names
-  paths <- forM properNames $ \name -> do
+  paths <- forM (filter accept names) $ \name -> do
     -- path computed by pure function hence let
     let path = topDir </> name
     -- P/254 doesDirectoryExist is an action; its return type is 
@@ -22,12 +25,43 @@ getRecursiveContents topDir = do
       else return [path]
   return (concat paths)
 
+preFilter :: FilePath -> Bool
+preFilter filePath = filePath `notElem` [".", "..", ".git"]
+
+process :: FilePath -> IO WalkResult
+process pth = do
+  case (".hs" `isSuffixOf` pth) of
+    True -> do 
+      contents <- readFile pth
+      putStrLn $ pth ++ "   " ++ (show $ length (lines contents))
+    False -> return ()
+  return WalkResult
+
+data WalkResult = WalkResult
+walk :: FilePath -> IO WalkResult
+walk topDir = do
+  names <- getDirectoryContents topDir
+  forM_ (filter preFilter names) $ \name -> do
+    let pth = topDir </> name
+    isDirectory <- doesDirectoryExist pth
+    if isDirectory
+      then walk(pth)
+      else process(pth)
+  return WalkResult
+
 demoGetRecursiveContents :: IO ()
 demoGetRecursiveContents = do
   print "//// demo get recursive contents"
-  getRecursiveContents ".." >>= (print . take 10)
+  contents <- getRecursiveContents "../.."
+  print $ length contents
+
+demoWalk :: IO ()
+demoWalk = do
+  print "//// walk with filter"
+  result <- walk "../.."
+  return ()
 
 main :: IO ()
 main = do
   demoGetRecursiveContents
-
+  demoWalk
