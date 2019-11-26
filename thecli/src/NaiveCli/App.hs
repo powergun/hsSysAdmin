e812da10d26bda83d0370726e28bebd64489bd1a
Wei Ning <macgnw@gmail.com>
2019-11-27 08:27:14 +1100

    wip

--- thecli/src/NaiveCli/App.hs
+++ thecli/src/NaiveCli/App.hs
@@ -30,7 +30,7 @@ data Options = Options
     , oExcited    :: Bool
     , oStdIn      :: Bool
     , oFileToRead :: Maybe String
-    }
+    } deriving Show
 
 -- program
 
@@ -39,33 +39,71 @@ main = runProgram =<< parseCLI
 
 runProgram :: Options -> IO ()
 runProgram o =
-    putStr =<< (handleExcitedness o . handleCapitalization o <$> getSource o)
+    putStr =<< ( handleExcitedness o
+               . handleCapitalization o <$> getSource o)
 
 -- data retrieval and transformation
 
+{-
+either :: (a -> c) -> (b -> c) -> Either a b -> c ???
+-}
 getSource :: Options -> IO String
-getSource o = B.bool (either id id <$> loadContents o) getContents $ oStdIn o
+getSource o = B.bool
+              -- false: load from file or default content
+              (either id id <$> loadContents o)
+              -- true: load from stdin
+              getContents
+              $ oStdIn o
 
 handleCapitalization :: Options -> String -> String
-handleCapitalization o = B.bool id (map C.toUpper) $ oCapitalize o
+handleCapitalization o = B.bool
+                         -- false: return input as it is
+                         id
+                         -- true: character-wise capitalization
+                         (map C.toUpper)
+                         $ oCapitalize o
 
 handleExcitedness :: Options -> String -> String
-handleExcitedness o = B.bool id ("ZOMG " ++) $ oExcited o
+handleExcitedness o = B.bool
+                      -- false: return input as it is
+                      id
+                      -- true: modify the input
+                      (":LOL " ++)
+                      $ oExcited o
 
+defaultContent :: String
+defaultContent = "there is a cow\n"
+
+{-
+MY NOTE:
+maybe :: b -> (a -> b) -> Maybe a -> b ??
+-}
 loadContents :: Options -> IO (Either String String)
 loadContents o =
     maybe defaultResponse readFileFromOptions $ oFileToRead o
   where
     readFileFromOptions f = BF.first show <$> safeReadFile f
-    defaultResponse = return $ Right "This is fun!"
+    defaultResponse = return $ Right defaultContent
 
 -- CLI parsing
 
+{-
+MY NOTE:
+test in ghci (make sure Options type is showable)
+:l NaiveCli.App
+parseCLI
+-}
 parseCLI :: IO Options
 parseCLI = execParser (withInfo parseOptions "File Fun")
   where
+    withInfo :: Parser Options -> String -> ParserInfo Options
     withInfo opts h = info (helper <*> opts) $ header h
 
+{-
+MY NOTE:
+see http://hackage.haskell.org/package/optparse-applicative-0.15.1.0/docs/Options-Applicative.html
+for `switch`, `long` and their usage pattern
+-}
 parseOptions :: Parser Options
 parseOptions = Options
     <$> (switch $ long "capitalize")
