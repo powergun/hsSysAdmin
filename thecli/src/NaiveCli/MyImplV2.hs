8f652e042c9b0acc10bfe2630d7532e59df29cf2
Wei Ning <macgnw@gmail.com>
2019-11-29 09:00:08 +1100

    wip

--- thecli/src/NaiveCli/MyImplV2.hs
+++ thecli/src/NaiveCli/MyImplV2.hs
@@ -1,6 +1,7 @@
 module NaiveCli.MyImplV2 (main) where
 
 import           Control.Exception   (IOException, try)
+import           Data.Bifunctor      (first)
 import           Data.Bool           (bool)
 import           Data.Char           (toUpper)
 import           Options.Applicative (Parser, execParser, header, helper, info,
@@ -21,7 +22,12 @@ runProgram opts = print =<< handlers opts
 
 handlers :: Options -> IO String
 handlers opts = do
-  t <- getText opts
+  -- use <$> below because getText' returns IO (Either String String)
+  -- instead of Either String String;
+  -- for the latter case I can simply use application operator $ or paren
+  -- in this case I can use a separate step to extract the inner value
+  -- or use applicative op
+  t <- either id id <$> getText' opts
   let t' = ( handleCapitalize opts
            . handleExcited opts ) t
   return t'
@@ -35,6 +41,22 @@ handleExcited opts = bool id ("!!!! " ++) (oExcited opts)
 getText :: Options -> IO String
 getText opts = bool (readFileOrDefault opts) readStdin (oStdIn opts)
 
+getText' :: Options -> IO (Either String String)
+getText' opts = bool (maybe doDefault doReadFile fnMaybe) doReadStdin (oStdIn opts)
+  where
+    fnMaybe :: Maybe FilePath
+    fnMaybe = oFileToRead opts
+    doDefault :: IO (Either String String)
+    doDefault = return $ Right "/usr/bin/env perl"
+    doReadFile :: FilePath -> IO (Either String String)
+    doReadFile fn = do
+      r <- readFileSafe' fn
+      return $ first show r
+    doReadStdin :: IO (Either String String)
+    doReadStdin = do
+      r <- readStdin
+      return $ Right r
+
 readStdin :: IO String
 readStdin = getContents
 
@@ -43,6 +65,9 @@ readFileOrDefault opts =
   case (oFileToRead opts) of
     Just filename -> readFileSafe filename
     _             -> return "there is default"
+
+readFileSafe' :: FilePath -> IO (Either IOException String)
+readFileSafe' = try . readFile
 
 readFileSafe :: FilePath -> IO String
 readFileSafe filename = do
