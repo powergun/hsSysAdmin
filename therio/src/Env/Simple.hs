1aae9fbea7ba08c979bc18de540af7ee8d149b1f
Wei Ning <macgnw@gmail.com>
2020-01-07 20:29:23 +1100

    rio

--- therio/src/Env/Simple.hs
+++ therio/src/Env/Simple.hs
@@ -3,7 +3,34 @@
 
 module Env.Simple (demo) where
 
+import           Prelude   (putStrLn)
 import           RIO
+import           System.IO (hPutStrLn, stderr)
+
+demoPrintToDefault :: IO ()
+demoPrintToDefault = do
+  let code = "IDDQD"
+      useCode = ask >>= liftIO . putStrLn
+      useCodePlus = ask >>= liftIO . putStrLn . ("seta " ++ )
+  runRIO code $ do
+    useCode
+    useCodePlus
+
+data AppConf = AppConf
+  { confName :: !String
+  , confCode :: !String
+  }
+
+demoPrintToHandle :: IO ()
+demoPrintToHandle = do
+  let conf = AppConf "e1m1" "IDKFA"
+      useCode = asks confName >>= liftIO . (hPutStrLn stderr)
+      useCodePlus = asks confCode >>= liftIO . (hPutStrLn stderr) . ("seta " ++ )
+  runRIO conf $ do
+    useCode
+    useCodePlus
 
 demo :: IO ()
-demo = return ()
+demo = do
+  demoPrintToDefault
+  demoPrintToHandle
